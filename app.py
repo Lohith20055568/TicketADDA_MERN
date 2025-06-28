6388dad962e932b5974f81c4aa0f663b17d75cb3
20055568 <lohithbadam10@gmail.com>
2025-06-28 20:10:41 +0100

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from crypto_utils import generate_keys, derive_shared_key, encrypt_message, decrypt_message
-from auth_utils import generate_otp, send_otp_email, validate_otp
-import sqlite3
-import base64  # For encoding nonce
-
-app = Flask(__name__)
-app.secret_key = "your_super_secret_key"
